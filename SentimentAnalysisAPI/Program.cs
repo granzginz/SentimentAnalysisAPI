5300d141cd228e714e8037cb307f16d04111b9c5
granzginz <ganjar56@gmail.com>
2023-12-14 06:35:57 +0000

    Refactor

--- SentimentAnalysisAPI/Program.cs
+++ SentimentAnalysisAPI/Program.cs
@@ -1,15 +1,36 @@
-using Startups;
-
-internal class Program
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Startups.Startup;
+public class Program
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    public static void Main(string[] args)
+        {
+                var host = CreateHostBuilder(args).Build();
 
-        var app = builder.Build();
+                        using (var scope = host.Services.CreateScope())
+                                {
+                                            var services = scope.ServiceProvider;
+                                                        try
+                                                                    {
+                                                                                    var context = services.GetRequiredService<SentimentDbContext>();
+                                                                                                    context.Database.Migrate();
+                                                                                                                }
+                                                                                                                            catch (Exception ex)
+                                                                                                                                        {
+                                                                                                                                                        var logger = services.GetRequiredService<ILogger<Program>>();
+                                                                                                                                                                        logger.LogError(ex, "An error occurred while migrating the database.");
+                                                                                                                                                                                    }
+                                                                                                                                                                                            }
 
-        //app.UseStartup<Startup>();
+                                                                                                                                                                                                    host.Run();
+                                                                                                                                                                                                        }
 
-        app.Run();
-    }
-}
+                                                                                                                                                                                                            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                                                                                                                                                                                                                    Host.CreateDefaultBuilder(args)
+                                                                                                                                                                                                                                .ConfigureWebHostDefaults(webBuilder =>
+                                                                                                                                                                                                                                            {
+                                                                                                                                                                                                                                                            webBuilder.UseStartup<Startup>();
+                                                                                                                                                                                                                                                                        });
+                                                                                                                                                                                                                                                                        }
